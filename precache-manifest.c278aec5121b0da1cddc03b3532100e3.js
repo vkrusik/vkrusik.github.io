79bddc391e1ce14afc47030ce124763eca24b7ae
vkrusik <52889586+vkrusik@users.noreply.github.com>
2019-07-21 16:33:55 +0300

    Add files via upload

---
+++ precache-manifest.c278aec5121b0da1cddc03b3532100e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f5a99bde9fadcdec06f",
+    "url": "/static/css/main.65f2c704.chunk.css"
+  },
+  {
+    "revision": "8f5a99bde9fadcdec06f",
+    "url": "/static/js/main.30b6b065.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4e247f65dc2abec056ca",
+    "url": "/static/css/2.befb28c0.chunk.css"
+  },
+  {
+    "revision": "4e247f65dc2abec056ca",
+    "url": "/static/js/2.5d15e94b.chunk.js"
+  },
+  {
+    "revision": "8685427eeb4eb97c5ee0fa6f78c7e539",
+    "url": "/index.html"
+  }
+];
